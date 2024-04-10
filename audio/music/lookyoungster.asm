d61c7d6c34c004675b177682f4b00fda99446464
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2024-04-10 18:45:34 +0700

    add youngster encounter music

--- audio/music/lookyoungster.asm
+++ audio/music/lookyoungster.asm
@@ -5,292 +5,534 @@ Music_LookYoungster:
 	channel 3, Music_LookYoungster_Ch3
 
 Music_LookYoungster_Ch1:
-	tempo 118
+	tempo 128
 	volume 7, 7
-	stereo_panning FALSE, TRUE
-	duty_cycle 3
-	note_type 12, 10, 3
-	octave 3
-	note G_, 1
-	note G#, 1
-	note A_, 1
-	note A#, 1
-	volume_envelope 3, -6
-	note B_, 16
-	volume_envelope 12, 3
-	octave 4
-	note C_, 1
-	rest 3
-	note C_, 4
-	rest 2
-	note C_, 6
-	volume_envelope 11, 3
-.mainloop:
-	rest 2
-	octave 3
-	note G_, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note G_, 1
-	rest 1
-	octave 4
-	note D_, 1
-	rest 1
-	note C_, 1
-	rest 3
-	note C_, 1
-	rest 1
-	rest 16
-	rest 2
-	octave 3
-	note G_, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note G_, 1
-	rest 1
-	octave 4
-	note D_, 1
-	rest 1
-	note C_, 1
-	rest 3
-	note C_, 1
-	rest 1
-	rest 16
-	sound_loop 0, .mainloop
-
-	sound_ret ; unused
-
-Music_LookYoungster_Ch2:
+	note_type 12, 15, 0
+	sound_call .pattern0
+.loop
 	stereo_panning TRUE, FALSE
-	vibrato 18, 2, 6
-	duty_cycle 1
-	note_type 12, 10, 3
+	sound_call .pattern1
+	sound_call .pattern3
+	sound_jump .loop
+
+.pattern0
+	duty_cycle 3
+	note_type 12, 10, 1
+	stereo_panning TRUE, FALSE
+	octave 3
+	note C_, 1
+	note D#, 1
+	note F#, 1
+	note G#, 1
+	octave 4
+	note C_, 1
+	note C_, 1
+	rest 2
+	stereo_panning FALSE, TRUE
+	octave 2
+	note A#, 1
+	octave 3
+	note C#, 1
+	note E_, 1
+	note G#, 1
+	stereo_panning TRUE, TRUE
+	volume_envelope 10, 3
+	note F#, 1
+	note G#, 1
+	note A#, 1
+	octave 4
+	note C_, 1
+	sound_ret
+
+.pattern1
+	note_type 12, 10, 1
+	octave 4
+	note C#, 2
+	octave 3
+	note G#, 2
+	note F_, 2
+	note G#, 2
+	note G#, 1
+	note A#, 1
+	note G#, 1
+	note F#, 1
+	volume_envelope 10, 3
+	note F_, 4
+	volume_envelope 10, 1
+	note F_, 1
+	note F#, 1
+	note G#, 1
+	note A#, 1
+	volume_envelope 10, 3
+	note G#, 4
+	volume_envelope 10, 1
+	note F#, 1
+	note F#, 1
+	note C#, 1
+	rest 1
+	volume_envelope 10, 3
+	note F#, 2
+	note A#, 2
+	duty_cycle 0
+	volume_envelope 10, 2
+	note G#, 1
+	note G#, 1
+	note C_, 2
+	note D#, 1
+	note D#, 1
+	note G#, 2
+	note G_, 1
+	note F#, 1
+	note E_, 1
+	note D#, 1
+	note E_, 1
+	note E_, 1
+	note D#, 1
+	note E_, 1
+	note F_, 1
+	note F_, 1
+	rest 2
+	note E_, 1
+	note E_, 1
+	rest 2
+	note D#, 1
+	note D#, 1
+	rest 1
+	octave 4
+	note C_, 1
+	note C_, 1
+	rest 1
+	octave 3
+	note G#, 2
+	sound_ret
+
+.pattern3
+	pitch_offset 1
+	rest 6
+	duty_cycle 3
+	octave 4
+	note F_, 2
+	note C#, 1
+	rest 1
+	octave 3
+	note G#, 1
+	octave 4
+	note C#, 1
 	octave 3
 	note B_, 1
+	octave 4
+	note C#, 1
+	octave 3
+	note B_, 1
+	note A#, 1
+	note G#, 1
+	rest 1
+	note C#, 1
+	rest 1
+	note G#, 1
+	note A#, 1
+	note B_, 1
+	octave 4
+	note C#, 1
+	octave 3
+	note A#, 1
+	rest 1
+	note F#, 1
+	rest 1
+	note C#, 1
+	rest 1
+	note E_, 1
+	rest 1
+	note G#, 1
+	note G#, 1
+	note E_, 1
+	note C_, 1
+	note G#, 1
+	note G#, 1
+	note E_, 1
+	note C_, 1
+	note C_, 1
+	note C_, 1
+	note C#, 1
+	note C#, 1
+	note D_, 1
+	note D_, 1
+	note C#, 1
+	note C#, 1
+	note_type 12, 10, 1
 	octave 4
 	note C_, 1
 	note C#, 1
 	note D_, 1
-	volume_envelope 3, -6
-	note_type 12, 2, -6
-	note G_, 16
-	note_type 12, 11, 3
-	volume_envelope 12, 3
-	note F_, 1
-	rest 3
-	note F_, 4
-	rest 2
-	note F#, 6
-.mainloop:
-	duty_cycle 3
-	volume_envelope 11, 3
-	rest 2
-	octave 3
-	note B_, 1
-	rest 1
-	octave 4
-	note D_, 1
-	rest 1
-	octave 3
-	note B_, 1
-	rest 1
-	octave 4
-	note G_, 1
-	rest 1
-	note F_, 1
-	rest 3
-	note F_, 1
-	rest 1
-	volume_envelope 9, 7
-	duty_cycle 2
 	note D#, 1
-	note F_, 1
+	note E_, 1
 	note D#, 1
 	note D_, 1
+	note C#, 1
 	note C_, 1
-	rest 1
-	octave 3
-	note G#, 1
-	rest 3
-	note F_, 1
-	rest 1
-	note G_, 1
-	rest 1
-	note G#, 1
-	rest 1
-	duty_cycle 3
-	volume_envelope 11, 3
-	rest 2
-	note B_, 1
-	rest 1
-	octave 4
 	note D_, 1
-	rest 1
+	note D#, 2
 	octave 3
-	note B_, 1
-	rest 1
-	octave 4
-	note G_, 1
-	rest 1
-	note F_, 1
-	rest 3
-	note F_, 1
-	rest 1
-	duty_cycle 2
-	volume_envelope 9, 7
-	note G#, 1
-	note A#, 1
-	note G#, 1
-	note G_, 1
-	note F_, 1
-	rest 1
 	note C_, 1
-	rest 3
-	octave 3
-	note G#, 1
-	rest 1
-	note A#, 1
-	rest 1
-	octave 4
 	note C_, 1
 	rest 1
-	sound_loop 0, .mainloop
+	volume_envelope 10, 2
+	note C_, 1
+	sound_ret
 
-	sound_ret ; unused
+Music_LookYoungster_Ch2:
+	note_type 12, 15, 0
+	sound_call .pattern0
+.loop
+	sound_call .pattern1
+	sound_call .pattern2
+	sound_jump .loop
+
+.pattern0
+	duty_cycle 2
+	note_type 12, 11, 1
+	octave 4
+	note G#, 1
+	note G#, 1
+	rest 1
+	note G#, 1
+	volume_envelope 11, 3
+	note G#, 4
+	volume_envelope 11, 1
+	note F#, 1
+	note F#, 1
+	rest 1
+	note F#, 1
+	volume_envelope 11, 3
+	note F#, 4
+	sound_ret
+
+.pattern1
+	duty_cycle 3
+	note_type 12, 12, 2
+	octave 4
+	note F_, 2
+	note C#, 2
+	octave 3
+	note G#, 1
+	note G#, 1
+	octave 4
+	note C#, 1
+	note C#, 1
+	octave 3
+	note B_, 1
+	octave 4
+	note C#, 1
+	octave 3
+	note B_, 1
+	note A#, 1
+	volume_envelope 12, 3
+	note G#, 3
+	rest 1
+	volume_envelope 12, 2
+	note G#, 1
+	note A#, 1
+	note B_, 1
+	octave 4
+	note C#, 1
+	volume_envelope 12, 3
+	octave 3
+	note B_, 4
+	note A#, 1
+	note A#, 1
+	octave 4
+	note C#, 1
+	rest 1
+	octave 3
+	note A#, 1
+	note A#, 1
+	octave 4
+	note E_, 1
+	note E_, 1
+	note D#, 1
+	note D#, 1
+	octave 3
+	note G#, 2
+	octave 4
+	note C_, 1
+	note C_, 1
+	note D#, 2
+	note C#, 1
+	octave 3
+	note B_, 1
+	note A#, 1
+	note G#, 1
+	note G_, 1
+	note G_, 1
+	note F#, 1
+	note G_, 1
+	note G#, 1
+	note G#, 1
+	rest 2
+	note G#, 1
+	note G#, 1
+	rest 2
+	note G#, 1
+	note G#, 1
+	rest 1
+	octave 4
+	note G#, 1
+	note G#, 1
+	rest 1
+	note F#, 2
+	sound_ret
+
+.pattern2
+	duty_cycle 2
+	note_type 12, 12, 2
+	octave 4
+	note F_, 2
+	note C#, 2
+	octave 3
+	note G#, 2
+	octave 4
+	note C#, 2
+	octave 3
+	note B_, 1
+	octave 4
+	note C#, 1
+	octave 3
+	note B_, 1
+	note A#, 1
+	volume_envelope 12, 3
+	note G#, 4
+	volume_envelope 12, 1
+	note G#, 1
+	note A#, 1
+	note B_, 1
+	octave 4
+	note C#, 1
+	volume_envelope 12, 3
+	octave 3
+	note B_, 4
+	note A#, 2
+	octave 4
+	note C#, 1
+	rest 1
+	octave 3
+	note F#, 1
+	rest 1
+	note A#, 1
+	rest 1
+	volume_envelope 12, 1
+	note G#, 1
+	note G#, 1
+	rest 2
+	note G#, 1
+	note G#, 1
+	rest 2
+	note G_, 1
+	note G_, 1
+	note G#, 1
+	note G#, 1
+	note A_, 1
+	note A_, 1
+	note G#, 1
+	note G#, 1
+	rest 4
+	volume_envelope 10, 7
+	octave 4
+	note E_, 1
+	note F_, 1
+	note F#, 1
+	note G_, 1
+	volume_envelope 12, 1
+	note G#, 1
+	note G#, 1
+	rest 1
+	note G#, 1
+	note F#, 1
+	note F#, 1
+	rest 1
+	note F#, 1
+	sound_ret
 
 Music_LookYoungster_Ch3:
 	stereo_panning TRUE, TRUE
-	vibrato 2, 2, 4
-	note_type 12, 2, 5
-	rest 4
-	octave 2
-	note G_, 1
-	rest 1
-	octave 3
-	note G_, 1
-	rest 1
-	octave 2
-	note G_, 1
-	rest 1
-	octave 3
-	note G_, 1
-	rest 1
-	octave 2
-	note G_, 1
-	rest 1
-	octave 3
-	note G_, 1
-	rest 1
-	volume_envelope 1, 5
-	octave 2
-	note G_, 1
-	rest 1
-	octave 3
-	note G_, 1
-	rest 1
-	note D_, 1
-	rest 3
-	note D_, 4
-	rest 2
-	note G#, 6
-	volume_envelope 2, 5
-.mainloop:
-	octave 2
-	note G_, 1
-	rest 1
-	octave 3
-	note G_, 1
-	rest 1
-	octave 2
-	note G_, 1
-	rest 1
-	octave 3
-	note G_, 1
-	rest 1
-	octave 2
-	note G_, 1
-	rest 1
-	octave 3
-	note G_, 1
-	rest 1
-	octave 2
-	note G_, 1
-	rest 1
-	octave 3
-	note G_, 1
-	rest 1
-	octave 2
-	note F_, 1
-	rest 1
-	octave 3
-	note D#, 1
-	rest 1
-	octave 2
-	note F_, 1
-	rest 1
-	octave 3
-	note D#, 1
-	rest 1
-	octave 2
-	note F_, 1
-	rest 1
-	octave 3
-	note D#, 1
-	rest 1
-	octave 2
-	note F_, 1
-	rest 1
-	octave 3
-	note D#, 1
-	rest 1
-	octave 2
-	note G_, 1
-	rest 1
-	octave 3
-	note G_, 1
-	rest 1
-	octave 2
-	note G_, 1
-	rest 1
-	octave 3
-	note G_, 1
-	rest 1
-	octave 2
-	note G_, 1
-	rest 1
-	octave 3
-	note G_, 1
-	rest 1
-	octave 2
-	note G_, 1
-	rest 1
-	octave 3
-	note G_, 1
-	rest 1
-	octave 2
-	note G#, 1
-	rest 1
-	octave 3
-	note D#, 1
-	rest 1
-	octave 2
-	note G#, 1
-	rest 1
-	octave 3
-	note D#, 1
-	rest 1
-	octave 2
-	note G#, 1
-	rest 1
-	octave 3
-	note D#, 1
-	rest 1
-	note G_, 1
-	rest 1
-	note G#, 1
-	rest 1
-	sound_loop 0, .mainloop
+	sound_call .pattern0
+.loop
+	stereo_panning FALSE, TRUE
+	sound_call .pattern1
+	stereo_panning TRUE, TRUE
+	sound_call .pattern2
+	sound_jump .loop
 
-	sound_ret ; unused
+.pattern0
+	note_type 12, 1, 5
+	octave 3
+	note G#, 1
+	note G#, 1
+	rest 1
+	note G#, 1
+	octave 2
+	note G#, 1
+	note G#, 3
+	octave 3
+	note F#, 1
+	note F#, 1
+	rest 1
+	note F#, 1
+	octave 2
+	note F#, 4
+	sound_ret
+
+.pattern1
+	note_type 12, 1, 5
+	octave 2
+	note G#, 1
+	rest 1
+	octave 3
+	note C#, 1
+	rest 1
+	octave 2
+	note G#, 1
+	rest 1
+	octave 3
+	note F_, 1
+	rest 1
+	octave 2
+	note B_, 1
+	rest 1
+	octave 3
+	note D#, 1
+	rest 1
+	octave 2
+	note B_, 1
+	rest 1
+	octave 3
+	note F#, 1
+	rest 1
+	octave 2
+	note G#, 1
+	rest 1
+	octave 3
+	note C#, 1
+	rest 1
+	octave 2
+	note G#, 1
+	rest 1
+	octave 3
+	note F_, 1
+	rest 1
+	note F#, 2
+	rest 1
+	note F#, 3
+	note A#, 2
+	note G#, 2
+	rest 1
+	note G#, 2
+	rest 1
+	octave 4
+	note C_, 1
+	rest 1
+	octave 2
+	note G_, 1
+	note G#, 1
+	note A#, 1
+	octave 3
+	note C#, 1
+	note D#, 1
+	note E_, 1
+	note F_, 1
+	note G_, 1
+	note G#, 1
+	note G#, 1
+	octave 2
+	note G#, 1
+	rest 1
+	octave 3
+	note G#, 1
+	note G#, 1
+	octave 2
+	note G#, 1
+	rest 1
+	note G#, 1
+	note A#, 1
+	octave 3
+	note C_, 1
+	octave 2
+	note C#, 1
+	note D#, 1
+	note F#, 1
+	note G#, 1
+	octave 3
+	note C_, 1
+	sound_ret
+
+.pattern2
+	note_type 12, 1, 4
+	octave 2
+	note C#, 2
+	octave 3
+	note C#, 1
+	rest 1
+	octave 2
+	note C#, 2
+	octave 3
+	note C#, 1
+	rest 1
+	octave 2
+	note D#, 2
+	octave 3
+	note D#, 1
+	rest 1
+	octave 2
+	note D#, 2
+	octave 3
+	note D#, 1
+	rest 1
+	octave 2
+	note C#, 2
+	octave 3
+	note C#, 1
+	rest 1
+	octave 2
+	note C#, 2
+	octave 3
+	note C#, 1
+	octave 2
+	note C#, 1
+	note F#, 4
+	note A#, 1
+	rest 1
+	octave 3
+	note C#, 1
+	note E_, 1
+	note G#, 2
+	rest 1
+	note G#, 1
+	octave 2
+	note G#, 4
+	octave 3
+	note A_, 2
+	rest 1
+	note A_, 1
+	octave 2
+	note A_, 4
+	note G#, 1
+	note A_, 1
+	note A#, 1
+	note B_, 1
+	octave 3
+	note C_, 1
+	octave 2
+	note B_, 1
+	note A#, 1
+	note A_, 1
+	note G#, 1
+	octave 3
+	note D#, 2
+	octave 2
+	note G#, 1
+	octave 3
+	note G_, 1
+	note G#, 1
+	octave 2
+	note G#, 1
+	note C_, 1
+	sound_ret
